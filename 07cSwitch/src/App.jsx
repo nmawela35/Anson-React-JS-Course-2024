2f219a1298f05fae89608424fe489aaffd7a9ebb
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-15 15:03:26 +0200

    Conditional Rendering,switch statements

--- 07cSwitch/src/App.jsx
+++ 07cSwitch/src/App.jsx
@@ -1,14 +1,34 @@
 export default function App() {
-  const isAuthenticated = true;
+  const USER_STATUS = "ACCOUNT_DISABLED";
 
-  return isAuthenticated ? (
-    <div>
-      <h1>Welcome User!</h1>
-    </div>
-  ) : (
-    <div>
-      <h1>Please login</h1>
-    </div>
-  );
+  switch (USER_STATUS) {
+    
+    case "NOT_VERIFIED":
+      return (
+        <div>
+          <h3>You are not verified</h3>
+        </div>
+      );
+
+    case "VERIFIED":
+      return (
+        <div>
+          <h3>You are verified, congrats</h3>
+        </div>
+      );
+
+    case "ACCOUNT_DISABLED":
+      return (
+        <div>
+          <h3>Account Disabled</h3>
+        </div>
+      );
+
+    default:
+      return (
+        <div>
+          <h3>Contact System administrator</h3>
+        </div>
+      );
+  }
 }
- 
