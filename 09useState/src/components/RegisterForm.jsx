51826460e35246bc8b324966246bad5318857eef
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-16 05:54:50 +0200

    useState added

---
+++ 09useState/src/components/RegisterForm.jsx
@@ -0,0 +1,20 @@
+export default function RegisterForm() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="" id="username" />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="text" name="" id="password" />
+      </div>
+
+      <div>
+        <label htmlFor="displayName">Display Name</label>
+        <input type="text" name="" id="displayName" />
+      </div>
+    </form>
+  );
+}
